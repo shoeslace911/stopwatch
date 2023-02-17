b2be4e1e2f1f2575c86e5ea3de68efa696cd2b20
Sho Fujiwara <shofujiwara505@gmail.com>
2023-02-17 14:04:09 +0900

    start button is starting

--- js.js
+++ js.js
@@ -1,17 +1,27 @@
 let secondsTime = document.getElementById("seconds");
 let milliSecondsTime = document.getElementById("milliseconds");
 let minutesTime = document.getElementById("minutes");
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+let lapButton = document.getElementById("lap");
 
+// <button id="start">Start</button>
+// <button id="stop">Stop</button>
+// <button id="lap">Lap</button>
 let milliSeconds = 0;
 let seconds = 0;
 let minutes = 0;
 
-setInterval(() => {
-  milliSeconds++;
-  milliSecondsTime.innerHTML = milliSeconds;
-  secondsTime.innerHTML = seconds;
-  minutesTime.innerHTML = minutes;
-  milliSeconds =
-    milliSeconds == 99 ? ((milliSeconds = 0), seconds++) : milliSeconds++;
-  seconds = seconds == 60 ? ((seconds = 0), minutes++) : seconds++;
-}, 1000 / 500);
+let counter = () => {
+  setInterval(() => {
+    milliSeconds++;
+    milliSecondsTime.innerHTML = milliSeconds;
+    secondsTime.innerHTML = seconds;
+    minutesTime.innerHTML = minutes;
+    milliSeconds =
+      milliSeconds == 99 ? ((milliSeconds = 0), seconds++) : milliSeconds++;
+    seconds = seconds == 60 ? ((seconds = 0), minutes++) : seconds++;
+  }, 1000 / 100);
+};
+
+startButton.addEventListener("click", counter);
